7d2dafc67616ed3d28e1379ba194f7c37d85be3d
DURGA9819 <suzanxtha123@gmail.com>
2024-02-27 13:21:28 +0545

    django

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-
 
 def home(request):
     return HttpResponse("""
@@ -21,3 +19,20 @@ def home(request):
 
 def root_page(request):
     return render(request, template_name="myapp/root_page.html")
+
+
+def temp_inherit_home(request):
+    return render(request, template_name="myapp/temp_inherit_home.html")
+
+def portfolio(request):
+    return render(request, template_name="myapp/portfolio.html")
+
+def classroom(request):
+    classrooms=[
+        {"name": "one", "address":"KTM"},
+        {"name": "two", "address":"BTM"},
+        {"name": "three", "address":"TTM"},
+        {"name": "four", "address":"RTM"},
+        {"name": "five", "address":"SPT"},
+    ]
+    return render(request, template_name="myapp/classroom.html", context={"classroom_name": "one", "location":"ktm", "classrooms":classrooms})
